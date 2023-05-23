53912541b43af424cbf540e9bb6afb12e9c5a077
Darshan Panchal <darshan.panchal@einfochips.com>
2023-05-23 15:45:47 +0530

    Priority Inversion

--- PriorityInversion.playground/Contents.swift
+++ PriorityInversion.playground/Contents.swift
@@ -2,28 +2,33 @@ import UIKit
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
-var greeting = "Hello, playground"
 
-let highPriority = DispatchQueue.global(qos: .userInitiated)
-let lowPriority = DispatchQueue.global(qos: .utility)
+let userInteractive = DispatchQueue.global(qos: .userInteractive)
+let userInitiated = DispatchQueue.global(qos: .userInitiated)
 let defaultPriority = DispatchQueue.global(qos: .default)
+let utility = DispatchQueue.global(qos: .utility)
+let background = DispatchQueue.global(qos: .background)
+let unspecified = DispatchQueue.global(qos: .unspecified)
 
 let semaPhore = DispatchSemaphore.init(value: 1)
-
 
 func printEmoji(queue:DispatchQueue,emoji:String){
     queue.async {
         debugPrint("\(emoji) waiting")
         semaPhore.wait()
-        for i in 0...10{
+        for i in 0...3{
             debugPrint("\(emoji) \(i)")
         }
         debugPrint("\(emoji) signal")
         semaPhore.signal()
     }
 }
-printEmoji(queue:lowPriority,emoji:"🚗")
-printEmoji(queue:highPriority,emoji:"🚑")
-printEmoji(queue: defaultPriority, emoji: "🚙") //low priority task execure first
+printEmoji(queue: userInitiated, emoji:"🚜")
+printEmoji(queue: userInteractive, emoji:"🚒")
+printEmoji(queue: defaultPriority, emoji:"🚚")
+printEmoji(queue:utility,emoji:"🚗")
+printEmoji(queue: background, emoji:"🚙")
+printEmoji(queue: unspecified, emoji:"🛵")
+
 
  
